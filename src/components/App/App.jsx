779c98c0d64ea9781f4bdf8dd99167946dc8ae0c
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-09-10 08:57:52 -0700

    app.jsx and some styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import styles from './App.css';
+import { BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+import Home from '../Home/Home';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Router className={styles.App}>
+        <Switch>
+          <Link exact path ="/" component={Home}/>
+        </Switch>
+      </Router>
+    </>
+  );
 }
-  
