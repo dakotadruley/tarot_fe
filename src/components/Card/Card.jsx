9148126b66fcf33b0e725c38571aefe86253133e
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-09-13 19:05:59 -0700

    added card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Card = ({ title, image, upright, reversed }) => (
+  <section>
+    <h3>{title}</h3>
+    <img src={image} />
+    <p>{upright}</p>
+    <p>{reversed}</p>
+  </section>
+);
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  upright: PropTypes.string.isRequired,
+  reversed: PropTypes.string.isRequired
+};
+
+export default Card;
